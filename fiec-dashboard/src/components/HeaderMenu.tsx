abfdd54cfeef837c473a5599ce3db24168d15bd6
Gustavo Lemos <gsvieira08@gmail.com>
2024-06-18 17:13:15 -0300

    Dropdown fechando quando ocorre um clique fora dele.

--- fiec-dashboard/src/components/HeaderMenu.tsx
+++ fiec-dashboard/src/components/HeaderMenu.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-
+import {
+  React,
+  useState,
+  useEffect,
+  useRef
+} from 'react';
 
 const HeaderMenu = ({ label, color, backgroundColor, items }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -12,6 +17,24 @@ const HeaderMenu = ({ label, color, backgroundColor, items }) => {
   const handleItemClick = (item) => {
     setSelectedItem(item);
   };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (dropdownOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownOpen]);
 
   return (
     <div className="relative flex justify-center">
@@ -24,7 +47,10 @@ const HeaderMenu = ({ label, color, backgroundColor, items }) => {
       </button>
 
       {dropdownOpen && items && items.length > 0 && (
-        <div className="absolute bg-white min-w-64 shadow-md rounded-lg mt-12 py-2 px-4">
+        <div
+          ref={dropdownRef}
+          className="absolute bg-white min-w-64 shadow-md rounded-lg mt-12 py-2 px-4"
+        >
           {items.map((item, index) => (
             <div key={index} className="mb-2">
               <div className="flex items-center cursor-pointer">
@@ -39,7 +65,7 @@ const HeaderMenu = ({ label, color, backgroundColor, items }) => {
                 <label
                   className={`flex items-center ${
                     selectedItem === item ? 'text-black' : 'text-gray-400'
-                  }`}
+                    }`}
                 >
                   <span className="px-4">{item.label}</span>
                 </label>
@@ -62,7 +88,7 @@ const HeaderMenu = ({ label, color, backgroundColor, items }) => {
                       <label
                         className={`flex items-center ${
                           selectedItem === subItem ? 'text-black' : 'text-gray-400'
-                        }`}
+                          }`}
                       >
                         <span className = "px-4">{subItem.label}</span>
                       </label>
