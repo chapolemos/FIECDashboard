ec020af41d29053c5d0fb2b6a836f7f7a89138b5
Gustavo Lemos <gsvieira08@gmail.com>
2024-06-18 09:55:04 -0300

    Navegação de abas inicial

--- fiec-dashboard/src/App.jsx
+++ fiec-dashboard/src/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import {
+  React,
+  useState
+} from 'react';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
+  const [activeTab, setActiveTab] = useState('tab1'); 
+
+
   return (
     <div className="flex h-screen w-screen bg-white">
       <main className="flex-1 flex">
-        <Sidebar />
-        <div className="flex-1 bg-gray-200 p-4">
+        <Sidebar setActiveTab={setActiveTab} />
+        <div className="flex-1 bg-black p-4">
           <h1 className="text-2xl font-bold mb-4">Conteúdo Aqui</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero vel
-            turpis viverra, sed vehicula est ullamcorper.
-          </p>
+          {activeTab === 'ranking' && (
+            <p>
+              Conteúdo da Aba 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero vel
+              turpis viverra, sed vehicula est ullamcorper.
+            </p>
+          )}
+          {activeTab === 'perfil' && (
+            <p>
+              Conteúdo da Aba 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero vel
+              turpis viverra, sed vehicula est ullamcorper.
+            </p>
+          )}
+          {activeTab === 'compare' && (
+            <p>
+              Conteúdo da Aba 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero vel
+              turpis viverra, sed vehicula est ullamcorper.
+            </p>
+          )}
         </div>
       </main>
     </div>
--- fiec-dashboard/src/components/Sidebar.tsx
+++ fiec-dashboard/src/components/Sidebar.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { colorTheme } from '../theme';
 import { abdi, fiec, indice22, obs, p2i } from '../assets/logos/index';
 
-const Sidebar = () => {
+const Sidebar = ({setActiveTab}) => {
   const { colors } = colorTheme;
 
   return (
-    <div style={{ backgroundColor: colors.sidebar }} className="w-48 h-screen flex flex-col justify-between">
+    <div style={{ backgroundColor: colors.sidebar }} className="w-48 h-screen flex flex-col justify-between max-h-screen">
       <div className="mt-2">
         <img src={indice22} alt="Logo do Indice FIEC de Inovação dos Estados 22" className="mb-2" />
       </div>
-      <div id = "navigationTabs"></div>
+
+      <div id="navigationTabs" className="flex flex-col items-center mb-auto">
+        <button className="bg-transparent text-white px-4 py-2 rounded mb-2 "
+          style={{ color: colors.text }}
+          onClick={() => setActiveTab('ranking')}
+        >Ranking</button>
+        <button className="bg-transparent text-white px-4 py-2 rounded mb-2"
+          style={{ color: colors.text }}
+          onClick={() => setActiveTab('perfil')}
+        >Perfil</button>
+        <button className="bg-transparent text-white px-4 py-2 rounded mb-2"
+          style={{ color: colors.text }}
+          onClick={() => setActiveTab('compare')}
+        >Compare</button>
+      </div>
+
       <div id="sidebarSubsection" className="flex flex-col justify-end">
         <img src={p2i} alt="Logo Programa de Inovação Industrial" className="mb-2" />
         <img src={obs} alt="Logo do Observatório da Indústria do Ceará" className="mb-2" />
