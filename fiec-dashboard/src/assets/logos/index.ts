2639ec8c59aadbff7a9dd7bde0d91f23ac89d50f
Gustavo Lemos <gsvieira08@gmail.com>
2024-06-18 09:33:38 -0300

    Indexando logos na pasta logos

---
+++ fiec-dashboard/src/assets/logos/index.ts
@@ -0,0 +1,7 @@
+import abdi from './abdi.png';
+import fiec from './fiec.png';
+import indice22 from './indice22.png';
+import obs from './obs.png';
+import p2i from './p2i.png';
+
+export { abdi, fiec, indice22, obs, p2i };
