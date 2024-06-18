53ab2611c5ce8fe059f62056ac977c6cb09f66ff
Gustavo Lemos <gsvieira08@gmail.com>
2024-06-17 21:58:13 -0300

    Sidebar inicial e container de conteudo principal

---
+++ fiec-dashboard/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+      <div className="w-48 bg-gray-800 h-screen">
+        <p className="text-white text-center pt-4">Sidebar</p>
+      </div>   
+  );
+};
+
+export default Sidebar;
