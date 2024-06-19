3228c8cdcc3a69222e11e10012330a074f382363
Gustavo Lemos <gsvieira08@gmail.com>
2024-06-19 16:47:49 -0300

    Menus sendo preenchidos pelo objeto

--- fiec-dashboard/src/pages/Ranking.tsx
+++ fiec-dashboard/src/pages/Ranking.tsx
@@ -1,154 +1,16 @@
 import React, { useState } from 'react';
-import {
-  Header,
-  HeaderCard,
-  HeaderMenu,
-  BrazilMap,
-  BarChart
-} from '../components';
+import { Header, HeaderCard, HeaderMenu, BrazilMap, BarChart } from '../components';
 import { colorTheme } from '../theme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
 import { regioes } from '../data/estadosDados';
 
-export const estoRank = [
+const estoRank = [
   { UF: 'SP', PosicaoRanking: 1, ValorRanking: 0.796 },
   { UF: 'SC', PosicaoRanking: 2, ValorRanking: 0.508 },
   { UF: 'RS', PosicaoRanking: 3, ValorRanking: 0.448 },
   { UF: 'TO', PosicaoRanking: 27, ValorRanking: 0.072 }
 ];
-
-const mockRegioes = [
-  {
-    nome: 'Nordeste',
-  },
-  {
-    nome: 'Centro-Oeste',
-  },
-  {
-    nome: 'Norte',
-  },
-  {
-    nome: 'Sudeste',
-  },
-  {
-    nome: 'Sul',
-  },
-];
-
-const data = [
-  {
-    nome: "Sudeste",
-    estados: [
-      {
-        nome: "São Paulo",
-        sigla: "SP"
-      },
-      {
-        nome: "Rio de Janeiro",
-        sigla: "RJ"
-       
-      },
-      {
-        sigla:"MG"
-      },
-      {
-        sigla:"ES"
-      },
-
-    ]
-  },
-  {
-    nome: "Nordeste", 
-    estados: [
-      {
-        sigla: "CE"
-      },
-      {
-        sigla: "RN"
-      },
-      {
-        sigla: "SE"
-      },
-      {
-        sigla: "PB"
-      },
-      {
-        sigla: "PE"
-      },
-      {
-        sigla: "AL"
-      },
-      {
-        sigla: "BA"
-      },
-      {
-        sigla: "PI"
-      },
-      {
-        sigla: "MA"
-      },
-    ]
-    }
-    ,
-    {
-      nome: "Sul", 
-      estados: [
-        {
-          sigla: "SC"
-        },
-        {
-          sigla: "RS"
-        },
-        {
-          sigla: "PR"
-        }
-      ]
-    },
-    {
-      nome: "Centro-oeste", 
-      estados: [
-        {
-          sigla: "MT"
-        },
-        {
-          sigla: "MS"
-        },
-        {
-          sigla: "DF"
-        },
-        {
-          sigla: "GO"
-        }
-      ]
-    },
-    {
-      nome: "Norte", 
-      estados: [
-        {
-          sigla: "AM"
-        },
-        {
-          sigla: "PA"
-        },
-        {
-          sigla: "RR"
-        },
-        {
-          sigla: "RO"
-        },
-        {
-          sigla: "AC"
-        },
-        {
-          sigla: "AP"
-        },
-        {
-          sigla: "TO"
-        }
-      ]
-    }
-  ];
 
 const Ranking = () => {
   const { colors } = colorTheme;
@@ -175,13 +37,13 @@ const Ranking = () => {
           label="Selecione um Indicador"
           color={colors.White}
           backgroundColor={colors.DodgerBlue}
-          items={mockRegioes}
+          items={regioes.map(regiao => ({ nome: regiao.nome }))}
         />
         <HeaderMenu
           label="Tipo de Regionalização"
           color={colors.White}
           backgroundColor={colors.Purple}
-          items={mockRegioes}
+          items={regioes.map(regiao => ({ nome: regiao.nome }))}
           onRegionSelect={handleRegionSelect}
         />
       </Header>
@@ -194,12 +56,12 @@ const Ranking = () => {
         }}
         >Índice FIEC de inovação</h2>
       </div>
+
       <div className="flex flex-row">
         <BrazilMap highlightedStates={highlightedStates}></BrazilMap>
         <div>
           <div className='bg-white px-10 py-2 rounded-xl'><BarChart data={estoRank} /></div>
-      
-    </div>
+        </div>
       </div>
     </>
   );
